db77f4305adb77cc12e83c3c3ca992c66e2df0c1
Melber17 <63192915+Melber17@users.noreply.github.com>
2021-01-01 19:22:18 +0300

    import-export

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+ export const Header = () => {
+    return <header className="header">
+        <img src="https://www.apple.com/v/apple-events/home/n/images/overview/meta/og__fodnljjkwl6y.png?202011090231" alt="apple" />
+    </header>   
+    
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const Navbar = () => {
+    return <nav className="nav">
+        <div>
+        <a href="#index">Profile</a>
+        </div>
+        <div>
+        <a href="#index">Messages</a>
+        </div>
+        <div>
+        <a href="#index">News</a>
+        </div>
+        <div>
+        <a href="#index">Music</a>
+        </div>
+        <div>
+        <a href="#index">Settings</a>
+        </div>
+      </nav>
+    
+}
