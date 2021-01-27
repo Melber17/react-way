6bbe0db885b90f84825477a9f36673a56e04f45e
Melber17 <63192915+Melber17@users.noreply.github.com>
2021-01-27 19:00:11 +0300

    auth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import s from "./Header.module.scss"
- export const Header = () => {
+import {NavLink} from "react-router-dom";
+
+export const Header = (props) => {
     return <header className={s.header}>
-        <img src="https://www.apple.com/v/apple-events/home/n/images/overview/meta/og__fodnljjkwl6y.png?202011090231" alt="apple" />
-    </header>  
- 
+        <img
+            src="https://www.apple.com/v/apple-events/home/n/images/overview/meta/og__fodnljjkwl6y.png?202011090231"
+            alt="apple"/>
+
+            <div className={s.coverLogin} >
+                {props.isAuth ? props.login : <NavLink className={s.loginBlock} to={'/login'}>Login</NavLink>}
+
+            </div>
+    </header>
+
 }
